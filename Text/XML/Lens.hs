b26d83105e308ebc1ff2a5b6d723e879907bea10
Fumiaki Kinoshita <fumiexcel@gmail.com>
2021-01-05 21:38:26 +0900

    Inline optics

--- Text/XML/Lens.hs
+++ Text/XML/Lens.hs
@@ -65,35 +65,43 @@ import Data.Maybe (isNothing)
 
 prologue :: Lens' Document Prologue
 prologue f doc = fmap (\p -> doc { documentPrologue = p} ) $ f $ documentPrologue doc
+{-# INLINE prologue #-}
 
 -- | The root element of the document.
 root :: Lens' Document Element
 root f doc = fmap (\p -> doc { documentRoot = p} ) $ f $ documentRoot doc
+{-# INLINE root #-}
 
 epilogue :: Lens' Document [Miscellaneous]
 epilogue f doc = fmap (\p -> doc { documentEpilogue = p} ) $ f $ documentEpilogue doc
+{-# INLINE epilogue #-}
 
 doctype :: Lens' Prologue (Maybe Doctype)
 doctype f doc = fmap (\p -> doc { prologueDoctype = p} ) $ f $ prologueDoctype doc
+{-# INLINE doctype #-}
 
 class AsInstruction t where
     _Instruction :: Prism' t Instruction
 
 _instructionTarget :: Lens' Instruction Text
 _instructionTarget f (Instruction t d) = f t <&> \t' -> Instruction t' d
+{-# INLINE _instructionTarget #-}
 
 _instructionData :: Lens' Instruction Text
 _instructionData f (Instruction t d) = f d <&> \d' -> Instruction t d'
+{-# INLINE _instructionData #-}
 
 instance AsInstruction Node where
     _Instruction = prism' NodeInstruction $ \s -> case s of
         NodeInstruction e -> Just e
         _ -> Nothing
+    {-# INLINE _Instruction #-}
 
 instance AsInstruction Miscellaneous where
     _Instruction = prism' MiscInstruction $ \s -> case s of
         MiscInstruction e -> Just e
         _ -> Nothing
+    {-# INLINE _Instruction #-}
 
 class AsComment t where
     _Comment :: Prism' t Text
@@ -102,33 +110,41 @@ instance AsComment Node where
     _Comment = prism' NodeComment $ \s -> case s of
         NodeComment e -> Just e
         _ -> Nothing
+    {-# INLINE _Comment #-}
 
 instance AsComment Miscellaneous where
     _Comment = prism' MiscComment $ \s -> case s of
         MiscComment e -> Just e
         _ -> Nothing
+    {-# INLINE _Comment #-}
 
 _nameLocalName :: Lens' Name Text
 _nameLocalName f n = f (nameLocalName n) <&> \x -> n { nameLocalName = x }
+{-# INLINE _nameLocalName #-}
 
 _nameNamespace :: Lens' Name (Maybe Text)
 _nameNamespace f n = f (nameNamespace n) <&> \x -> n { nameNamespace = x }
+{-# INLINE _nameNamespace #-}
 
 _namePrefix :: Lens' Name (Maybe Text)
 _namePrefix f n = f (namePrefix n) <&> \x -> n { namePrefix = x }
+{-# INLINE _namePrefix #-}
 
 _Element :: Prism' Node Element
 _Element = prism' NodeElement $ \s -> case s of
     NodeElement e -> Just e
     _ -> Nothing
+{-# INLINE _Element #-}
 
 _Content :: Prism' Node Text
 _Content = prism' NodeContent $ \s -> case s of
     NodeContent e -> Just e
     _ -> Nothing
+{-# INLINE _Content #-}
 
 name :: Lens' Element Name
 name f e = f (elementName e) <&> \x -> e { elementName = x }
+{-# INLINE name #-}
 
 localName :: Lens' Element Text
 localName = name . _nameLocalName
@@ -136,21 +152,26 @@ localName = name . _nameLocalName
 
 attrs :: Lens' Element (Map Name Text)
 attrs f e = fmap (\x -> e { elementAttributes = x }) $ f $ elementAttributes e
+{-# INLINE attrs #-}
 
 nodes :: Lens' Element [Node]
 nodes f e = fmap (\x -> e { elementNodes = x }) $ f $ elementNodes e
+{-# INLINE nodes #-}
 
 attr :: Name -> Traversal' Element Text
 attr n = attrs . ix n
+{-# INLINE attr #-}
 
 attribute :: Name -> Lens' Element (Maybe Text)
 attribute n = attrs . at n
+{-# INLINE attribute #-}
 
 -- | Traverse elements which has the specified *local* name (case-insensitive).
 named :: CI.CI Text -> Traversal' Element Element
 named n f s
     | CI.mk (nameLocalName (elementName s)) == n = f s
     | otherwise = pure s
+{-# INLINE named #-}
 
 -- | Old name for 'named'
 ell :: Text -> Traversal' Element Element
@@ -165,24 +186,31 @@ el n f s
 
 attributeSatisfies :: Name -> (Text -> Bool) -> Traversal' Element Element
 attributeSatisfies n p = attributeSatisfies' n (maybe False p)
+{-# INLINE attributeSatisfies #-}
 
 attributeSatisfies' :: Name -> (Maybe Text -> Bool) -> Traversal' Element Element
 attributeSatisfies' n p = filtered (p . preview (attrs . ix n))
+{-# INLINE attributeSatisfies' #-}
 
 withoutAttribute :: Name -> Traversal' Element Element
 withoutAttribute n = attributeSatisfies' n isNothing
+{-# INLINE withoutAttribute #-}
 
 attributeIs :: Name -> Text -> Traversal' Element Element
 attributeIs n v = attributeSatisfies n (==v)
+{-# INLINE attributeIs #-}
 
 -- | Traverse all contents of the element.
 text :: Traversal' Element Text
 text = nodes . traverse . _Content
+{-# INLINE text #-}
 
 -- | Traverse all comments of the element.
 comment :: Traversal' Element Text
 comment = nodes . traverse . _Comment
+{-# INLINE comment #-}
 
 -- | 'plate' traverses over its sub-elements.
 instance Plated Element where
     plate = nodes . traverse . _Element
+    {-# INLINE plate #-}
